96632dbdf4cb9a6d8915397cd6c2c3344ddc7e63
Sparsh Saxena <90604939+SPARSH511@users.noreply.github.com>
2023-09-11 22:24:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-const addButton = document.getElementById('addButton');
-const taskInput = document.getElementById('taskInput');
-const taskList = document.getElementById('taskList');
 
-addButton.addEventListener('click', () => {
-    const taskText = taskInput.value.trim();
-    if (taskText !== '') {
-        const taskItem = document.createElement('li');
-        taskItem.innerHTML = `
-            ${taskText}
-            <button class="completeButton">Complete</button>
-        `;
-        taskList.appendChild(taskItem);
-        taskInput.value = '';
-        attachCompleteHandler(taskItem);
+document.addEventListener('DOMContentLoaded', () => {
+    const addButton = document.getElementById('addButton');
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+
+    addButton.addEventListener('click', () => {
+        const taskText = taskInput.value.trim();
+        if (taskText !== '') {
+            const taskItem = document.createElement('li');
+            taskItem.innerHTML = `
+                ${taskText}
+                <button class="completeButton">Complete</button>
+            `;
+            taskList.appendChild(taskItem);
+            taskInput.value = '';
+            attachCompleteHandler(taskItem);
+        }
+    });
+
+    function attachCompleteHandler(taskItem) {
+        const completeButton = taskItem.querySelector('.completeButton');
+        completeButton.addEventListener('click', () => {
+            taskItem.classList.toggle('completed');
+        });
     }
 });
-
-function attachCompleteHandler(taskItem) {
-    const completeButton = taskItem.querySelector('.completeButton');
-    completeButton.addEventListener('click', () => {
-        taskItem.classList.toggle('completed');
-    });
-}
